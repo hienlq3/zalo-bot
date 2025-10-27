d0b0f96a01e692dbb59f75cde0090a73d1efe377
adminit <lequyhien2908@gmail.com>
2025-10-27 15:45:53 +0700

    added logs

--- zalo.js
+++ zalo.js
@@ -4,9 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export async function sendZaloMessage(userId, text) {
-    const url = process.env.ZALO_API_URL;
+    const url = process.env.ZALO_API_URL || "https://openapi.zalo.me/v3.0/oa/message/cs";
     console.log("Sending Zalo message to URL:", url);
     const accessToken = process.env.ZALO_ACCESS_TOKEN;
+    console.log("Sending Zalo message to accessToken:", accessToken);
 
     const data = {
         recipient: { user_id: userId },
