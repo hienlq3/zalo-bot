d003b06d7db8af05f04aedb86465e1e111fb237e
root <lequyhien2908@gmail.com>
2025-10-31 13:12:44 +0700

    updated nginx

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.get("/", (req, res) => {
+app.get("/zalo", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
